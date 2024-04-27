f5dcf9a2e0c5d063e7e8a505b5e66b138f1e0bde
AgustinBeniteez <adivinanzas1991@gmail.com>
2024-04-27 20:05:42 +0200

    animation txt

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+    var dataText = ["Utrecht.", "Full Service.", "Webdevelopment.", "Wij zijn Codefield!"];
+
+    function typeWriter(text, i, fnCallback) {
+        if (i < (text.length)) {
+            document.querySelector(".typing-effect").innerHTML = text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
+            setTimeout(function() {
+                typeWriter(text, i + 1, fnCallback)
+            }, 100);
+        } else if (typeof fnCallback == 'function') {
+            setTimeout(fnCallback, 700);
+        }
+    }
+
+    function startTextAnimation(i) {
+        if (typeof dataText[i] == 'undefined') {
+            setTimeout(function() {
+                startTextAnimation(0);
+            }, 20000);
+        }
+        if (i < dataText.length) {
+            typeWriter(dataText[i], 0, function() {
+                startTextAnimation(i + 1);
+            });
+        }
+    }
+
+    startTextAnimation(0);
+});
